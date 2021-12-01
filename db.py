83bc702f8153f59a34c3d3d58046c1e1ba31cf1f
vovan <vovan@nimble.com>
2021-12-01 12:46:41 +0100

    2nd

--- db.py
+++ db.py
@@ -9,8 +9,8 @@ def process_input(total_input, db_dict):
         print("please use allowed command from:", allowed_list )
         return False
     
-    if command == "SET":
-        
+    #process SET
+    if command == "SET":      
         if len(total_input.split()) != 3:
             print("please write proper SET command, e.g. SET a 10")
             return False
@@ -19,12 +19,23 @@ def process_input(total_input, db_dict):
 
         return db_dict
 
+    #process GET
+    if command == "GET":       
+        if len(total_input.split()) != 2:
+            print("please write proper GET command, e.g. GET a ")
+            return False
         
-    
-    
+        try:
+            print(db_dict[total_input.split()[1]])
+            return True
+        except KeyError:
+            print("please use proper variable")
+            return False
+       
+           
 db_space = {}
 
 while True:
     myinput = input(">>>").strip()
     process_input(myinput, db_space)
-    print(db_space)
+    
